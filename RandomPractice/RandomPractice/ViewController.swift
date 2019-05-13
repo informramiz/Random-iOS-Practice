689c8fe7eaf0f9c318fc6e3244f79164b27f913f
Ramiz Raja <informramiz@gmail.com>
2019-05-13 09:19:01 +0500

    chore: Create project

---
+++ RandomPractice/RandomPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomPractice
+//
+//  Created by Apple on 13/05/2019.
+//  Copyright © 2019 RR Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
