4af09f907887d73fc9a7a97969edf10ab51233c0
Ramiz Raja <informramiz@gmail.com>
2019-05-13 09:42:04 +0500

    feat: Add data source to table view

--- RandomPractice/RandomPractice/ViewController.swift
+++ RandomPractice/RandomPractice/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: Model
+    let favoriteThings = [
+        "Raindrops on roses",
+        "Whiskers on kittens",
+        "Bright copper kettles",
+        "Warm woolen mittens"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //register data source and delegates for table view
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableViewCell = tableView.dequeueReusableCell(withIdentifier: "FavoriteThingsCell")
+        tableViewCell?.textLabel?.text = favoriteThings[indexPath.row]
+        return tableViewCell!
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favoriteThings.count
+    }
 }
 
