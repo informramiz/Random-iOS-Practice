9041cc86be5c393719aabf2ac3ccbe2e930f3bb0
Ramiz Raja <informramiz@gmail.com>
2019-08-27 06:20:19 +0500

    feat: Practice file read/write

--- RandomPractice/RandomPractice/controller/UserDefaultsTestViewController.swift
+++ RandomPractice/RandomPractice/controller/UserDefaultsTestViewController.swift
@@ -23,11 +23,36 @@ class UserDefaultsTestViewController: UIViewController {
         super.viewDidLoad()
         themeSwitchView.isOn = MyUserDefaults.isDarkThemeOn()
         updateTheme()
+        testFileReadWrite()
     }
     
     @IBAction func onThemeChange(_ sender: Any) {
         UserDefaults.standard.set(themeSwitchView.isOn, forKey: MyUserDefaults.UserDefaultsKeys.isDarkThemeOn.rawValue)
         updateTheme()
+    }
+    
+    private func testFileReadWrite() {
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let url = urls.last!.appendingPathComponent("myFile.txt")
+        
+        //let's write some text to file
+        do {
+            try "Hi There".write(to: url, atomically: true, encoding: .utf8)
+        } catch {
+            print("Error occurred while writing text to file")
+        }
+        
+        //let's read the text from file to make sure it was written correctly
+        do {
+            let content = try String(contentsOf: url, encoding: .utf8)
+            if content == "Hi There" {
+                print("File write and read was done correctly")
+            } else {
+                print("There was some issue in either writing the file or reading the file")
+            }
+        } catch {
+            print("Error occurred while reading from file")
+        }
     }
     
     /*
